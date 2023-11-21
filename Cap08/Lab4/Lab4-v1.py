5d208a6bd4b49f1c30773c10f01b9c982a1b6d75
yngridcfsouza <isnotlive.yngrid@gmail.com>
2023-11-21 16:00:25 -0300

    Caps 07 ao 10
    Exercícios e projetos

---
+++ Cap08/Lab4/Lab4-v1.py
@@ -0,0 +1,96 @@
+# Hangman Game (Jogo da Forca) 
+# Programação Orientada a Objetos
+
+# Import
+import random
+
+# Board (tabuleiro)
+board = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+
+# Classe
+class Hangman:
+
+	# Método Construtor
+     def __init__(self):
+          
+	     # Windows
+	     if name == "nt":
+		     _= system('cls')
+	     # MAC ou Linux
+	     else: 
+		     _= system('clear')
+     
+          # Printa boas vindas
+	     print("\nBem-vindo(a) ao jogo da forca!")
+	     print("Adivinhe a palavra a seguir:\n")
+          
+
+	# Método para adivinhar a letra
+	
+	# Método para verificar se o jogo terminou
+		
+	# Método para verificar se o jogador venceu
+		
+	# Método para não mostrar a letra no board
+		
+	# Método para checar o status do game e imprimir o board na tela
+
